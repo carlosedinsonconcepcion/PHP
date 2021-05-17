4c882e9d618289944cb9a165f55c00a0b73b7d78
carlosedinsonconcepcion <Carlosedinsonconcepcion@gmail.com>
2021-05-17 12:51:09 -0400

    Agrego mis primeros archivos del curso PHP a mi repositorio local

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/* Asignacion aritmetica */
+
+$num = 9;
+
+$lang = [
+    'es'=> 'español'
+    'en'=> 'ingles'
+];
+
+/* Aritmetica */
+
+
+
+class answer
+{
+    protected $clients = []; # Mi comentario
+    
+    protected $insurers = []; // Mi comentario
+}

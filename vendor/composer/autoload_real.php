b27bb5d685987a4f10f5b97a64e41948f86f808f
carlosedinsonconcepcion <Carlosedinsonconcepcion@gmail.com>
2021-05-18 13:08:52 -0400

    clase no 13 descargando paquetes composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit8369b9b6b73c0691edcd0d34658f8249
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit8369b9b6b73c0691edcd0d34658f8249', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit8369b9b6b73c0691edcd0d34658f8249', 'loadClassLoader'));
